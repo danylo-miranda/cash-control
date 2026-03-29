327a0b4b307a8b965d8b50a0d89ea6e16984d350
Danylo Miranda <danylo.s.miranda@gmail.com>
2026-03-29 16:06:12 -0300

    fix: sqlite

--- src/cash_control/infrasctructure/repositories/user_repository_impl.py
+++ src/cash_control/infrasctructure/repositories/user_repository_impl.py
@@ -4,17 +4,31 @@ from cash_control.domain.interfaces.user_repository import UserRepository
 from cash_control.infrasctructure.models.user_models import UserModel
 
 class UserRepositoryImpl(UserRepository):
-    
+
     def __init__(self, db: Session):
         self.db = db
-        
-        def create(self, user: User):
-            model = UserModel(
-                name=user.name,
-                email=user.email,
-                password=user.hashed_password
-            )
-            self.db.add(model)
-            self.db.commit()
-            self.db.refresh(model)
-            return User
+
+    def create(self, user: User) -> User:
+        model = UserModel(
+            name=user.name,
+            email=user.email,
+            password=user.hashed_password
+        )
+
+        self.db.add(model)
+        self.db.commit()
+        self.db.refresh(model)
+
+        return user  # pode evoluir depois pra retornar ID
+
+    def get_by_email(self, email: str) -> User | None:
+        model = self.db.query(UserModel).filter(UserModel.email == email).first()
+
+        if not model:
+            return None
+
+        return User(
+            name=model.name,
+            email=model.email,
+            hashed_password=model.password
+        )
--- src/cash_control/main.py
+++ src/cash_control/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from cash_control.api.v1.routes.user_routes import router
+from cash_control.core.database import Base, engine
 
 app = FastAPI()
+
+# cria as tabelas automaticamente
+Base.metadata.create_all(bind=engine)
 
 app.include_router(router, prefix="/api/v1")
